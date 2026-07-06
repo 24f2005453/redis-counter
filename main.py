e937ff2b73383c5abd55c7229ac17a6e6b6cfc04
Jason Cornelius J <24f2005453@ds.study.iitm.ac.in>
2026-07-06 20:25:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+import redis
+
+app = FastAPI()
+
+r = redis.Redis(
+    host="redis",
+    port=6379,
+    decode_responses=True
+)
+
+
+@app.post("/hit/{key}")
+def hit(key: str):
+    count = r.incr(key)
+    return {
+        "key": key,
+        "count": count
+    }
+
+
+@app.get("/count/{key}")
+def count(key: str):
+    value = r.get(key)
+
+    if value is None:
+        value = 0
+    else:
+        value = int(value)
+
+    return {
+        "key": key,
+        "count": value
+    }
+
+
+@app.get("/healthz")
+def health():
+    try:
+        r.ping()
+        return {
+            "status": "ok",
+            "redis": "up"
+        }
+    except Exception:
+        return {
+            "status": "error",
+            "redis": "down"
+        }
